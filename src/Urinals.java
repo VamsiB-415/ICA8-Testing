5eea3b2095fa5d78db99a8702f7adb21581a0bbd
Naga Vamsi Bhargava Reddy Seelam (Student) <nseelam2@sundevils.asu.edu>
2022-10-27 22:29:13 -0700

    Added new junit for failed counturinals functions

--- src/Urinals.java
+++ src/Urinals.java
@@ -1,3 +1,9 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Title: ICA-8 Testing Counting Urinals
  *
@@ -8,10 +14,32 @@ public class Urinals {
     public static void main(String[] args)
     {
         Urinals urinals = new Urinals();
-        //System.out.println(urinals.countUrinals("1100001"));
-        //goodString("100010101");
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Press 1 for Keyboard input");
+        System.out.println("Press 2 for file input");
+        int choice=scan.nextInt();
+        String s="";
+        if(choice==1)
+        {
+            System.out.println("Enter your input");
+            s=scan.next();
+            int urinalCount=urinals.countUrinals(s);
+            if(urinalCount==-1)
+                System.out.println("String is Invalid");
+            else
+                System.out.println(urinalCount);
+        }
+        else if(choice == 2)
+        {
+
+        }
+        else
+        {
+            System.out.println("Enter only 1 or 2");
+        }
+
     }
-     Boolean goodString(String str)
+    Boolean goodString(String str)
     { // checks to see if valid string
        for(int i=0;i<str.length();i++)
        {
@@ -26,6 +54,11 @@ public class Urinals {
            }
        }
         return true;
+    }
+    int countUrinals(String str)
+    {
+        System.out.println("Not Yet Implemented");
+        return 0;
     }
 
 
--- src/urinalsTest.java
+++ src/urinalsTest.java
@@ -6,22 +6,27 @@ public class urinalsTest {
     @Test
     public void testgoodString1()
     {
-        System.out.println("------------------------Vamsi Bhargav==Test one executed----------------");
+        System.out.println("==========Vamsi Bhargav==Test one executed========");
         assertEquals(true,urinals.goodString("0001001"));
 
     }
     @Test
     public void testgoodString2()
     {
-        System.out.println("------------------------Vamsi Bhargav==Test one executed----------------");
+        System.out.println("==========Vamsi Bhargav==Test 2 executed========");
         assertEquals(false,urinals.goodString("000vamsi001"));
     }
     @Test
     public void testgoodString3()
     {
-        System.out.println("------------------------Vamsi Bhargav==Test one executed----------------");
+        System.out.println("==========Vamsi Bhargav==Test 3 executed========");
         assertEquals(true,urinals.goodString("0001001"));
-
+    }
+    @Test
+    public void testcountUrinals1()
+    {
+        System.out.println("==========Vamsi Bhargav==Test 4 executed========");
+        assertEquals(1,urinals.countUrinals("0001001"));
     }
 }
 
